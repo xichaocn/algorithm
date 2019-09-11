8be16ab64633f0bf6bc8ae33afbb7241287e720a
xichao <xichao@zhihu.com>
2019-09-11 15:15:58 +0800

    1、两数之和 2、两数相加 3、无重复字符的最长子串

---
+++ src/main/java/tech/xichao/AddTwoNumbers.java
@@ -0,0 +1,93 @@
+package tech.xichao;
+
+/**
+ * 两数相加
+ *
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 示例：
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ * @author xichao
+ * @date 20190910
+ */
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode result = addTwoNumbers(l1, l2);
+        System.out.println(listNodeToString(result));
+
+        ListNode l3 = new ListNode(5);
+        ListNode l4 = new ListNode(5);
+        result = addTwoNumbers(l3, l4);
+        System.out.println(listNodeToString(result));
+    }
+
+
+    /**
+     * 复杂度分析
+     * 时间复杂度：O(max(m,n))，假设 m 和 n 分别表示 l1 和 l2 的长度，上面的算法最多重复 max(m,n) 次。
+     * 空间复杂度：O(max(m,n))， 新列表的长度最多为 max(m,n) + 1
+     */
+    private static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode current = result;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int a = l1 != null ? l1.val : 0;
+            int b = l2 != null ? l2.val : 0;
+            int c = carry + a + b;
+            carry = c / 10;
+            current.val = c % 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+            if (l1 != null || l2 != null) {
+                current.next = new ListNode(0);
+                current = current.next;
+            }
+        }
+        if(carry > 0) {
+            current.next = new ListNode(carry);
+        }
+        return result;
+    }
+
+    private static String listNodeToString(ListNode node) {
+        if (node == null) {
+            return "[]";
+        }
+
+        String result = "";
+        while (node != null) {
+            result += Integer.toString(node.val) + ", ";
+            node = node.next;
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+}
---
+++ src/main/java/tech/xichao/LengthOfLongestSubstring.java
@@ -0,0 +1,96 @@
+package tech.xichao;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 无重复字符的最长子串
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ * 示例 1:
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ *
+ * 示例 2:
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ *
+ * 示例 3:
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ *
+ * @author xichao
+ * @date 20190910
+ */
+public class LengthOfLongestSubstring {
+
+    public static void main(String[] args) {
+//        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+//        System.out.println(lengthOfLongestSubstring("bbbbb"));
+//        System.out.println(lengthOfLongestSubstring("pwwkew"));
+//        System.out.println(lengthOfLongestSubstring("dvdf"));
+        System.out.println(lengthOfLongestSubstring2("abba"));
+        System.out.println(lengthOfLongestSubstring2("cbabcb"));
+    }
+
+    /**
+     * 暴力法非常简单，但它太慢了。那么我们该如何优化它呢？
+     * 在暴力法中，我们会反复检查一个子字符串是否含有有重复的字符，但这是没有必要的。如果从索引 i 到 j−1 之间的子字符串 s_{ij}
+     * 已经被检查为没有重复字符。我们只需要检查 s[j] 对应的字符是否已经存在于子字符串 s_{ij}中。
+     * 要检查一个字符是否已经在子字符串中，我们可以检查整个子字符串，这将产生一个复杂度为 O(n^2)的算法，但我们可以做得更好。
+     *
+     * 通过使用 HashSet 作为滑动窗口，我们可以用 O(1) 的时间来完成对字符是否在当前的子字符串中的检查。
+     * 滑动窗口是数组/字符串问题中常用的抽象概念。 窗口通常是在数组/字符串中由开始和结束索引定义的一系列元素的集合，即 [i, j)（左闭，右开）。
+     * 而滑动窗口是可以将两个边界向某一方向“滑动”的窗口。例如，我们将 [i, j) 向右滑动 1 个元素，则它将变为 [i+1, j+1)（左闭，右开）。
+     *
+     * 回到我们的问题，我们使用 HashSet 将字符存储在当前窗口 [i, j)（最初 j = i）中。 然后我们向右侧滑动索引 j，
+     * 如果它不在 HashSet 中，我们会继续滑动 j。直到 s[j] 已经存在于 HashSet 中。此时，我们找到的没有重复字符的最长子字符串将会以索引 i 开头。
+     * 如果我们对所有的 i 这样做，就可以得到答案。
+     *
+     */
+    private static int lengthOfLongestSubstring1(String s) {
+        Set<Character> set = new HashSet<>();
+        int ans = 0;
+        for(int i= 0, j=0, len = s.length(); j<len; j++) {
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 上述的方法最多需要执行 2n 个步骤。事实上，它可以被进一步优化为仅需要 n 个步骤。
+     * 我们可以定义字符到索引的映射，而不是使用集合来判断一个字符是否存在。当我们找到重复的字符时，我们可以立即跳过该窗口。
+     *
+     * 也就是说，如果 s[j] 在 [i, j) 范围内有与 j' 重复的字符，我们不需要逐渐增加 i 。
+     * 我们可以直接跳过 [i，j'] 范围内的所有元素，并将 i 变为 j' + 1 。
+     */
+    private static int lengthOfLongestSubstring2(String s) {
+        int ans = 0;
+        //创建map窗口,i为左区间，j为右区间，右边界移动
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i= 0, j=0, len = s.length(); j<len; j++) {
+            //如果窗口中包含当前字符，左边界移动到 相同字符的下一个位置和i当前位置中更靠右的位置，这样是为了防止i向左移动
+            if(map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)) + 1, i);
+            }
+            //比对当前无重复字段长度和储存的长度，选最大值并替换
+            //j-i+1是因为此时i,j索引仍处于不重复的位置，j还没有向后移动，取的[i,j]长度
+            ans = Math.max(ans, j - i + 1);
+            // 将当前字符为key，索引为value放入map中
+            map.put(s.charAt(j), j);
+        }
+        return ans;
+    }
+
+}
